ff727e6fad3a39d9b312221c1df54e9bd29773eb
Jo-fai Chow <jofai.chow@gmail.com>
2014-05-26 14:59:29 +0100

    Simplified the download process - EBImage::readImage() can read URL directly!

--- R/extract_colours.R
+++ R/extract_colours.R
@@ -3,9 +3,8 @@
 #' This function downloads an image and extracts the dominant colours from it. 
 #' The code is based on dsparks' gist (https://gist.github.com/dsparks/3980277).
 #' 
-#' @param url_img URL of the image (can be PNG, JPG, JPEG, TIFF)
+#' @param url_img URL of the image (can be PNG, JPG, JPEG, TIFF) or a local file
 #' @param num_col Number of dominant colours to be extracted (default = 5)
-#' @param nme_pal Name of the new palette (default = "custom_palette")
 #' 
 #' @examples
 #' Example 1a - Using the R logo to create a 5-colour palette
@@ -26,27 +25,12 @@
 #' pie(rep(1, 5), col = pal_s, main = "Palette based on Simpsons")
 #' hist(Nile, breaks = 5, col = pal_s, main = "Palette based on Simpsons")
 
-
-extract_colours <- function(url_img = "http://developer.r-project.org/Logo/Rlogo-1.png", 
-                     num_col = 5, nme_pal = "custom_palette") {
-  
-  ## Assign a temp file name based on URL
-  tmp_nme <- switch(str_sub(url_img, start = -3),
-                    jpg = "temp_pic.jpg",
-                    JPG = "temp_pic.jpg",
-                    epg = "temp_pic.jpg",
-                    EPG = "temp_pic.jpg",
-                    png = "temp_pic.png",
-                    PNG = "temp_pic.png",
-                    tiff = "temp_pic.tiff",
-                    TIFF = "temp_pic.tiff")
-  
-  ## Get a local copy
-  download.file(url_img, tmp_nme, mode = "wb")
+extract_colours <- function(
+  url_img = "http://developer.r-project.org/Logo/Rlogo-1.png", num_col = 5) {
   
   ## Read Image
-  img <- readImage(tmp_nme)
-    
+  img <- readImage(url_img) # local file or url
+  
   ## Resize Image (make it smaller so the remaining tasks run faster)  
   if (max(dim(img)[1:2]) > 100) {
     if (dim(img)[1] > dim(img)[2]) {
@@ -67,8 +51,7 @@ extract_colours <- function(url_img = "http://developer.r-project.org/Logo/Rlogo
   col_dom <- kmeans(img_rgb[, 3:5], centers = num_col, nstart = 5)
   
   ## Return k-means centers as RGB colours
-  cus_pal <- matrix(sort(rgb(col_dom$centers)))
-  colnames(cus_pal) <- nme_pal
-  return(cus_pal)
+  cus_pal <- sort(rgb(col_dom$centers))
+  return(as.character(cus_pal))
   
 }
