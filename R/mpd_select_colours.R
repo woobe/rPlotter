f24f4d5ded0adaed4e216bcdf7d4903e2aa4d131
rdinnager <r.dinnage@gmail.com>
2014-06-08 16:42:52 +0800

    added informative error if no colours are returned after thresholding and now palette lists are returned sorted by descending MPD.

--- R/mpd_select_colours.R
+++ R/mpd_select_colours.R
@@ -11,6 +11,10 @@ require(colorspace)
 #' @param light.thresh Maximum luminosity of colours in resulting palette (between 0 and 1)
 #' @param sat.thresh Minimum luminosity of colours in resulting palette (between 0 and 1)
 #' @param nreps The number of samples of the new colour palette to perform for brute force optimization of MPD
+#' @param ncolours The number of colours to select for the new palette
+#' @param nreturn The number of palettes to return
+#' \value{ If nreturn > 1 then a list of length nreturn, each element of which is a character vector of length ncolours consisting of hexidecimal colour codes, 
+#' else a character vector of length ncolours consisting of hexidecimal colour codes }
 mpd_select_colours <- function(pal, sat.thresh = NULL, light.thresh = NULL, dark.thresh = NULL, nreps = 10000, ncolours = ceiling(length(pal)/2), nreturn = 1) {
   if (ncolours > length(pal)) {
     stop("Number of colours to select must be less than the total number of colours in the palette")  
@@ -37,9 +41,12 @@ mpd_select_colours <- function(pal, sat.thresh = NULL, light.thresh = NULL, dark
   }
   ## get rid of dark colours if desired
   if (!is.null(dark.thresh)) {
-    ## convert light thresholds from proportions into L*A*B* scale (perceptual)
+    ## convert dark thresholds from proportions into L*A*B* scale (perceptual)
     dark.thresh <- ceiling(dark.thresh*100)
     cols.LAB <- cols.LAB[cols.LAB[,1] > dark.thresh,] 
+  }
+  if (is.null(nrow(cols.LAB))) {
+    stop("No colours after thresholding")  
   }
   if (nrow(cols.LAB) < 2) {
     stop("Too few colours after thresholding")  
@@ -59,12 +66,14 @@ mpd_select_colours <- function(pal, sat.thresh = NULL, light.thresh = NULL, dark
     new_pals_index <- apply(col_samp[col_unique,], 1, function(x) x==1)
     new_pals <- apply(new_pals_index, 2, function(x) rownames(new_pals_index)[x])
     new_pals <- lapply(seq_len(ncol(new_pals)), function(x) new_pals[,x])
+    new_pals <- new_pals[order(col_unique)]
   } else {
     col_unique <- col_unique[rank(-col_mpd[col_unique]) <= nreturn]
     if (nreturn > 1) {
       new_pals_index <- apply(col_samp[col_unique,], 1, function(x) x==1)
       new_pals <- apply(new_pals_index, 2, function(x) rownames(new_pals_index)[x])
       new_pals <- lapply(seq_len(ncol(new_pals)), function(x) new_pals[,x])
+      new_pals <- new_pals[order(col_unique)]
     } else {
       new_pals_index <- col_samp[col_unique,]==1
       new_pals <- names(new_pals_index)[new_pals_index]  
